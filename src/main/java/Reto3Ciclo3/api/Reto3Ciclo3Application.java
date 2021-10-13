0d2cc045c13e6d34f16b6816417f0c6e0f5a534f
Lina Maria Guacaneme Sanchez <lina.guacaneme.mt@correo.usa.edu.co>
2021-10-12 23:28:57 -0500

    Se crea Repositorio y se agrega el package del spring Reto3Ciclo3

---
+++ src/main/java/Reto3Ciclo3/api/Reto3Ciclo3Application.java
@@ -0,0 +1,13 @@
+package Reto3Ciclo3.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3Ciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Ciclo3Application.class, args);
+	}
+
+}
